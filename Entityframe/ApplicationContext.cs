7c3a84946b0eb7c6d8c2d58c86ccd63c9977c312
opilane <opilane@HP-CZC6297B8L>
2022-09-23 11:44:35 +0300

    sa

--- Entityframe/ApplicationContext.cs
+++ Entityframe/ApplicationContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
+using Entityframe;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +13,30 @@ namespace Entityframe
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<User> Users { get; set; }
         public ApplicationContext()
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Userf>().HasKey(u => new { u.PassportSeria, u.PassportNumber });
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=efbasicsappdb;Trusted_Connection=True;");
         }
     }
+    public class Userf
+    {
+        public string? PassportNumber { get; set; }
+        public string? PassportSeria { get; set; }
+
+        public string? Name { get; set; }
+    }
+
+
+
 
 }
--- Entityframe/User.cs
+++ Entityframe/User.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Entityframe
 {
     public class User
     {
-        public long Id { get; set; }
+        [Key]
+        public int Ident { get; set; }
         public string? Name { get; set; }
-
-      
-
-  
-        public long Age { get; set; }
-
-        public string? Position { get; set; }
-
-
     }
+
+
+
 }
